deea71ba6889919ea27420d601a778842cdf7884
angemiJR <111733781+angemiJR@users.noreply.github.com>
2024-10-28 11:59:53 +0100

    exercise 1 done

--- exercise1.js
+++ exercise1.js
@@ -1,13 +1,20 @@
-function guessNumberGame(){
+function guessNumberGame() {
 
-const number = Math.floor(Math.random()*10);
-let guess = null;
+    const number = Math.floor(Math.random() * 10);
+    let guess = null;
 
-while (guess !== number) {
-   
-    guess= parseFloat(prompt("Make a guess"));
-}
+    while (guess !== number) {
 
+        guess = parseFloat(prompt("Make a guess"));
+
+        if (guess > number) {
+            console.log("Guess is too high! Guess again.");
+        } else if (guess < number) {
+            console.log("Guess is too low! Guess again.");
+        } else {
+            console.log("Congrats you win!");
+        }
+    }
 }
 
 guessNumberGame();
