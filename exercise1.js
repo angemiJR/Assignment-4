d7458cf7b9c5988bc2ec93ebaf130278d3e1a58c
angemiJR <111733781+angemiJR@users.noreply.github.com>
2024-10-28 11:39:44 +0100

    while loop done

--- exercise1.js
+++ exercise1.js
@@ -3,5 +3,13 @@ function guessNumberGame(){
 const number = Math.floor(Math.random()*10);
 let guess = null;
 
+while (guess !== number) {
+   
+    guess= parseFloat(prompt("Make a guess"));
+}
 
 }
+
+guessNumberGame();
+
+
